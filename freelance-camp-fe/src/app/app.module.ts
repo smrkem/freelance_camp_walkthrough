2b72c0c292b5592f7b6de2de2b3413a637ee3a0c
matt <smrkem@gmail.com>
2017-06-06 15:50:44 -0400

    initial commit

---
+++ freelance-camp-fe/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { AppRoutingModule } from './app-routing.module';
+
+@NgModule({
+  imports:      [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  declarations: [
+    AppComponent,
+    HomepageComponent
+  ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
