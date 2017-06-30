6507cd495f0b31e298d0e78596e15ba32c664c5a
smrkem <smrkem@gmail.com>
2017-06-30 16:57:48 -0400

    starting DocumentsService

--- freelance-camp-fe/src/app/documents/documents.component.ts
+++ freelance-camp-fe/src/app/documents/documents.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Document } from './document';
+import { DocumentsService } from './documents.service';
 
 @Component({
   moduleId: module.id,
@@ -7,28 +8,11 @@ import { Document } from './document';
   templateUrl: 'documents.component.html',
   styleUrls: ['documents.component.css']
 })
-export class DocumentsComponent {
-  documents: Document[] = [
-    {
-      title: "My 1 Doc",
-      description: "Doc 1 description",
-      file_url: "http://example.com",
-      updated_at: "6/6/2017",
-      image_url: "https://thumb9.shutterstock.com/display_pic_with_logo/1700341/221855992/stock-photo-the-word-freelance-on-wood-stamp-stacking-on-desk-with-laptop-glasses-and-a-cup-of-coffee-at-home-221855992.jpg"
-    },
-    {
-      title: "My 2 Doc",
-      description: "Doc 2 description",
-      file_url: "http://example.com",
-      updated_at: "5/6/2017",
-      image_url: "https://thumb9.shutterstock.com/display_pic_with_logo/1700341/221855992/stock-photo-the-word-freelance-on-wood-stamp-stacking-on-desk-with-laptop-glasses-and-a-cup-of-coffee-at-home-221855992.jpg"
-    },
-    {
-      title: "My 3 Doc",
-      description: "Doc 3 description",
-      file_url: "http://example.com",
-      updated_at: "6/7/2017",
-      image_url: "https://thumb9.shutterstock.com/display_pic_with_logo/1700341/221855992/stock-photo-the-word-freelance-on-wood-stamp-stacking-on-desk-with-laptop-glasses-and-a-cup-of-coffee-at-home-221855992.jpg"
-    }
-  ]
+export class DocumentsComponent implements OnInit {
+  documents: Document[];
+  mode = "Observable";
+
+  constructor(
+    private documentsService: DocumentsService;
+  ) {}
 }
