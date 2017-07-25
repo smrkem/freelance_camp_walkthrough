3938908ab960a02a4d26a5cfcf7c8fa717fa4e93
matt <smrkem@gmail.com>
2017-07-25 18:17:05 -0400

    gets docs working

--- freelance-camp-fe/src/app/documents/documents.component.ts
+++ freelance-camp-fe/src/app/documents/documents.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Rx';
 import { Document } from './document';
 import { DocumentsService } from './documents.service';
 
@@ -6,13 +7,28 @@ import { DocumentsService } from './documents.service';
   moduleId: module.id,
   selector: 'documents',
   templateUrl: 'documents.component.html',
-  styleUrls: ['documents.component.css']
+  styleUrls: ['documents.component.css'],
+  providers: [ DocumentsService ]
 })
 export class DocumentsComponent implements OnInit {
   documents: Document[];
+  errorMessage: string;
   mode = "Observable";
 
   constructor(
-    private documentsService: DocumentsService;
+    private documentsService: DocumentsService
   ) {}
+
+  ngOnInit() {
+    let timer = Observable.timer(0, 5000);
+    timer.subscribe(() => this.getDocuments());
+  }
+
+  getDocuments() {
+    this.documentsService.getDocuments()
+      .subscribe(
+        documents => this.documents = documents,
+        error => this.errorMessage = <any>error
+      );
+  }
 }
